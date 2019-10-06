d44f34248cbef889c61eb799f6d1fda689b1633a
Samdark2000 <55610239+Samdark2000@users.noreply.github.com>
2019-10-06 17:25:26 -0400

    Add files via upload

---
+++ tarea2/tarea.php
@@ -0,0 +1,336 @@
+
+
+<Html>
+
+<head>
+       <title>Tabla Periodica de los Elementos Quimico</title>
+
+</head>
+
+<style>
+     body{
+         background-color: gray
+     }
+
+     .amarillo{
+        background-color: yellow
+     }
+
+     .verde{
+        background-color: #45B575
+     }
+
+     .moradoClaro{
+        background-color: #B67DB0
+     }
+
+     .orange{
+        background-color: #FBC14B
+     }
+
+
+     .moradoClarito{
+        background-color: #F09BF6
+     }
+     
+     .azulClarito{
+        background-color:#72A9E8
+     }
+     
+
+     .azulClaro{
+        background-color:#7FBFC6
+     }
+
+
+     .verdeClaro{
+        background-color:#C8DC64
+     }
+
+     .orangeClaro{
+        background-color:  #E59647
+     }
+
+     .salmon{
+        background-color:  #E27F86
+     }
+
+     .white{
+        background-color: white
+     }
+
+    
+
+</style>
+
+
+<body>
+<center><h1 style="color:DodgerBlue;"><i>Tabla Periodica de los Elementos Quimico</i></h1></center>
+
+
+<title>Tabla Peridica de los elementos quimico</title>
+
+<table class="tabla-simbolo"; border ="0" >
+
+        <tr><!--primera columna-->
+  
+ 
+            <td  class= "verde">1<br><strong>H</strong><br>Hidrogeno</td>
+        
+            <td colspan = "16"></td>
+
+            <td class = "azulClarito" >2<br><strong>He</strong><br>Helio </td>
+        </tr>
+
+    
+        <tr> <!--segunda fila-->
+  
+            <td class = "moradoClaro" >3<br><strong>Li</strong><br>Litio </td>
+            <td class = "moradoClarito" >4<br><strong>Be</strong><br>Berilio </td>
+ 
+               <td colspan = "10"></td>
+
+            <td  class= "verdeClaro">5<br><strong>B</strong><br>Boro</td>
+            <td  class= "verde">6<br><strong>C</strong><br>Carbono</td>
+            <td  class= "verde">7<br><strong>N</strong><br>Nitrogeno</td>
+            <td  class= "verde">8<br><strong>O</strong><br>Oxigeno</td>
+            <td class = "azulClaro" >9<br><strong>F</strong><br>Fluor </td>
+            <td class = "azulClarito" >10<br><strong>Ne</strong><br>Neon </td>
+
+
+         </tr>
+         <tr> <!--Tercera fila-->
+  
+  
+         <td class = "moradoClaro" >11<br><strong>Na</strong><br>Sodio </td>
+            <td class = "moradoClarito" >12<br><strong>Mg</strong><br>Magnesio </td>
+ 
+               <td colspan = "10"></td>
+
+               <td class = "amarillo" >13<br><strong>Al</strong><br>Aluminio </td>
+               <td  class= "verdeClaro">14<br><strong>Si</strong><br>Silicio</td>
+            <td  class= "verde">15<br><strong>P</strong><br>Fosforo</td>
+            <td  class= "verde">16<br><strong>S</strong><br>Asufre</td>
+            <td class = "azulClaro" >17<br><strong>Cl</strong><br>Cloro </td>
+            <td class = "azulClarito" >18<br><strong>Ar</strong><br>Argon </td>
+
+
+         </tr>
+
+         <tr> <!--cuarta fila-->
+  
+            <td class = "moradoClaro" >19<br><strong>K</strong><br>Potasio </td>
+            <td class = "moradoClarito" >20<br><strong>Ca</strong><br>Calcio </td>
+            <td class = "orange" >21<br><strong>Sc</strong><br>Escandio </td>
+            <td class = "orange" >22<br><strong>Ti</strong><br>Titanio </td>
+            <td class = "orange" >23<br><strong>V</strong><br>Vandadio </td>
+            <td class = "orange" >24<br><strong>Cr</strong><br>Cromo </td>
+            <td class = "orange" >25<br><strong>Mn</strong><br>Manganeso </td>
+            <td class = "orange" >26<br><strong>Fe</strong><br>Hierro </td>
+            <td class = "orange" >27<br><strong>Co</strong><br>Colbato </td>
+            <td class = "orange" >28<br><strong>Ni</strong><br>Niquel </td>
+            <td class = "orange" >29<br><strong>Cu</strong><br>Cobre </td>
+            <td class = "orange" >30<br><strong>Zn</strong><br>Zinc </td>
+            <td class = "amarillo" >31<br><strong>Ga</strong><br>Galio </td>
+            <td  class= "verdeClaro">32<br><strong>Ge</strong><br>Germanio</td>
+            <td  class= "verdeClaro">33<br><strong>As</strong><br>Arsenico</td>
+            <td  class= "verde">34<br><strong>Se</strong><br>Selenio</td>
+            <td class = "azulClaro" >35<br><strong>Br</strong><br>Bromo </td>
+            <td class = "azulClarito" >36<br><strong>Kr</strong><br>Kripton </td>
+
+
+         </tr>
+
+         
+         </tr>
+
+         <tr> <!--quinta fila-->
+  
+  
+            <td class = "moradoClaro" >37<br><strong>Rb</strong><br>Rubidio </td>
+            <td class = "moradoClarito" >38<br><strong>Sr</strong><br>Estroncio </td>
+            <td class = "orange" >39<br><strong>Y</strong><br>itrio </td>
+            <td class = "orange" >40<br><strong>Zr</strong> <br>Circonio </td>
+            <td class = "orange" >41<br><strong>Nb</strong> <br> Niobio</td>
+            <td class = "orange" >42<br><strong>Mo</strong> <br>Molibdeno </td>
+            <td class = "orange" >43<br><strong>Tc</strong> <br>Tecnecio </td>
+            <td class = "orange" >44<br><strong>Ru</strong> <br>Rutenio </td>
+            <td class = "orange" >45<br><strong>Rh</strong> <br>Rodio </td>
+            <td class = "orange" >46<br><strong>Pd</strong> <br>Paladio </td>
+            <td class = "orange" >47<br><strong>Ag</strong> <br>Plata </td>
+            <td class = "orange" >48<br><strong>Cd</strong> <br>Cadmio </td>
+            <td class = "amarillo" >49<br><strong>In</strong> <br> Indio </td>
+            <td class = "amarillo" >50<br><strong>Sn</strong> <br>Estaño </td>
+            <td class= "verdeClaro">51 <br><strong>Sb</strong> <br>Antimonio</td>
+            <td class= "verdeClaro">52<br><strong>Te</strong> <br>Telurio</td>
+            <td class = "azulClaro" >53<br><strong>I</strong> <br>Yodo </td>
+            <td class = "azulClarito" >54<br><strong>Xe</strong> <br>Xenon </td>
+
+
+         </tr>
+
+         
+         </tr>
+
+         <tr> <!--sesta fila-->
+  
+  
+            <td class = "moradoClaro" >55<br><strong>Cs</strong> <br>Cesio </td>
+            <td class = "moradoClarito" >56<br><strong>Ba</strong> <br>Barlo </td>
+            <td class = "orangeClaro" >57-71<br><strong>La-Lu</strong> <br> Lantanidos</td>
+            <td class = "orange" >72<br><strong>Hf</strong> <br> Hafnio</td>
+            <td class = "orange" >73<br><strong>Ta</strong> <br> Tantalo</td>
+            <td class = "orange" >74<br><strong>W</strong> <br> Wolframio </td>
+            <td class = "orange" >75<br><strong>Re</strong> <br> Renio  </td>
+            <td class = "orange" >76<br><strong>Os</strong> <br> Osmio</td>
+            <td class = "orange" >77<br><strong>Ir</strong> <br> iridio</td>
+            <td class = "orange" >78<br><strong>Pt</strong> <br> Platino</td>
+            <td class = "orange" >79<br><strong>Au</strong> <br> Oro </td>
+            <td class = "orange" >80<br><strong>Hg</strong> <br>Mercurio</td>
+            <td class = "amarillo" >81<br><strong>Ti</strong> <br>Tallo </td>
+            <td class = "amarillo" >82<br><strong>Pb</strong> <br> Plomo </td>
+            <td class = "amarillo" >83<br><strong>Bi</strong> <br>Bismuto</td>
+            <td  class= "verdeClaro">84<br><strong>Po</strong> <br>Polonio</td>
+            <td class = "azulClaro" >85<br><strong>At</strong> <br> Astato </td>
+            <td class = "azulClarito" >86<br><strong>RN</strong> <br>Radon</td>
+
+
+         </tr>
+
+         
+         </tr>
+
+         <tr> <!--septima fila-->
+  
+  
+         <td class = "moradoClaro" >87<br><strong>Fr</strong><br> Francio </td>
+            <td class = "moradoClarito" >88<br><strong>Ra</strong><br>Radio</td>
+            <td class = "salmon" >89-103<br><strong>Ac-Lr</strong><br>Actinidos </td>
+            <td class = "orange" >104<br><strong>Rf</strong><br>RUtherfordio </td>
+            <td class = "orange" >105<br><strong>Db</strong><br>Dubnio </td>
+            <td class = "orange" >106<br><strong>Sg</strong><br>Seaborgio </td>
+            <td class = "orange" >107<br><strong>Bh</strong><br>Bohrio </td>
+            <td class = "orange" >108<br><strong>Hs</strong><br>Hasio </td>
+            <td class = "orange" >109<br><strong>Mt</strong><br>Meitnerio </td>
+            <td class = "orange" >110<br><strong>Ds</strong><br>Darmstatio </td>
+            <td class = "orange" >111<br><strong>Rg</strong><br>Roentgenio </td>
+            <td class = "orange" >112<br><strong>Cn</strong><br>Copernicio </td>
+            <td class = "amarillo" >113<br><strong>Nh</strong><br>Nihonio </td>
+            <td class = "amarillo" >114<br><strong>Fl</strong><br>Flerovio </td>
+            <td class = "amarillo" >115<br><strong>Mc</strong><br>Moscovio </td>
+            <td class = "amarillo" >116<br><strong>Lv</strong><br>Livermorio </td>
+            <td class = "azulClaro" >117<br><strong>Ts</strong><br>Teneso</td>
+            <td class = "azulClarito" >118<br><strong>Og</strong><br>Oganeson </td>
+
+
+         </tr>
+
+
+</table >
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+
+<center>
+
+<table border ="0"; ALIGN=RIGHT >
+
+<tr> <!--ocho fila-->
+  
+  
+     <td class = "orangeClaro" >57<br><strong>La</strong><br>Lantano </td>
+     <td class = "orangeClaro" >58<br><strong>Ce</strong><br>Cerio </td>
+     <td class = "orangeClaro" >59<br><strong>Pr</strong><br>Praseodimio </td>
+     <td class = "orangeClaro" >60<br><strong>Nd</strong><br>Neodimio </td>
+     <td class = "orangeClaro" >61<br><strong>Pm</strong><br>Prometio </td>
+     <td class = "orangeClaro" >62<br><strong>Sm</strong><br>Samario </td>
+     <td class = "orangeClaro" >63<br><strong>Eu</strong><br> Europio </td>
+     <td class = "orangeClaro" >64<br><strong>Gd</strong><br>Gadolinio </td>
+     <td class = "orangeClaro" >65<br><strong>Tb</strong><br>Terbio </td>
+     <td class = "orangeClaro" >66<br><strong>Dy</strong><br>Disprosio </td>
+     <td class = "orangeClaro" >67<br><strong>Ho</strong><br>Holmio </td>
+     <td class = "orangeClaro" >68<br><strong>Er</strong><br>Erbio </td>
+     <td class = "orangeClaro" >69<br><strong>Tm</strong><br> Tulio </td>
+     <td class = "orangeClaro" >70<br><strong>Yb</strong><br> Iterbio </td>
+     <td class = "orangeClaro" >71<br><strong>Lu</strong><br> Lutecio </td>
+
+
+  </tr>
+
+  <tr> <!--nueve fila-->
+  
+  <td class = "salmon" >89<br><strong>Ac</strong><br>Actinio </td>
+  <td class = "salmon" >90<br><strong>Th</strong><br>Torio </td>
+  <td class = "salmon" >91<br><strong>Pa</strong><br>Protactinio </td>
+  <td class = "salmon" >92<br><strong>U</strong><br>Uranio </td>
+  <td class = "salmon" >93<br><strong>Np</strong><br> Neptunio </td>
+  <td class = "salmon" >94<br><strong>Pu</strong><br> Plutonio </td>
+  <td class = "salmon" >95<br><strong>Am</strong><br> Americio </td>
+  <td class = "salmon" >96<br><strong>Cm</strong><br>Curio </td>
+  <td class = "salmon" >97<br><strong>Bk</strong><br>Berkelio </td>
+  <td class = "salmon" >98<br><strong>Cf</strong><br>Californio </td>
+  <td class = "salmon" >99<br><strong>Es</strong><b>Einstenio </td>
+  <td class = "salmon" >100<br><strong>Fm</strong><br>Fermio </td>
+  <td class = "salmon" >101<br><strong>Md</strong><br> Mendelevio </td>
+  <td class = "salmon" >102<br><strong>No</strong><br> Nobello </td>
+  <td class = "salmon" >103<br><strong>Lr</strong><br> Lawrencio </td>
+
+
+  </tr>
+
+
+
+
+
+</table>
+</center>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+
+
+<br/>
+<br/>
+<br/>
+
+<center>
+<table  >
+  <tbody><tr>
+   <th colspan="5" class="White">Metales</a></th>
+   <td  class="verdeClaro" rowspan="3">Metaloides</a></td>
+   <th colspan="3" class="white">No Metales</a></th>
+  </tr><tr>
+   <td  class="moradoClaro" rowspan="2">Alcalinos</a></td>
+   <td  class="moradoClarito" rowspan="2">Alcalinotérreos</a></td>
+   <td  class="orange" rowspan="2">Metales de Transición/ Bloque D</a></td>
+   <td class="orangeClaro">Lantánidos</a></td>
+   <td  class="amarillo" rowspan="2">Otros Metales</a></td>
+   <td  class="verde" rowspan="2">Otros no Metales</a></td>
+   <td  class="azulClaro" rowspan="2">Halogenos</a></td>
+   <td  class="azulClarito" rowspan="2">Gases nobles</a></td>
+  </tr><tr>
+   <td id="Actinoid" class="salmon">Actínidos</a></td>
+  </tr></tbody>
+ </table>
+</center>
+
+
+?>
+
+
+</body>
+
+</html>
+
+
+
+
